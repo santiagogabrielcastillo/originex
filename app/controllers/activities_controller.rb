337f4fb334eb5f3dcb8f462bb033476a735b224d
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-01 17:01:58 -0300

    search working + activity seeds

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,5 @@
 class ActivitiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
     if params[:query].present?
       @activities = Activity.search_over_activities(params[:query])

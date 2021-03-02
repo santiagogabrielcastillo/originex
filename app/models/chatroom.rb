82d128a891ae1c0e2ad9ac08da40ce6de0f9ff3d
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-02 12:15:36 -0300

    adds chatroom controller + messages model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,6 @@
 class Chatroom < ApplicationRecord
   belongs_to :activity
+  has_many :messages
 
   validates :title, :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_201638) do
+ActiveRecord::Schema.define(version: 2021_03_02_151441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 2021_03_01_201638) do
     t.index ["chatroom_id"], name: "index_events_on_chatroom_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,4 +75,6 @@ ActiveRecord::Schema.define(version: 2021_03_01_201638) do
   add_foreign_key "chatrooms", "activities"
   add_foreign_key "events", "activities"
   add_foreign_key "events", "chatrooms"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end

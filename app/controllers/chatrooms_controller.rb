efec490d2dfecc8ccd1244a996226079fdc491cb
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-02 14:14:16 -0300

    adds chatrooms show and create

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,30 @@
 class ChatroomsController < ApplicationController
+  before_action :set_activity, only: %i[new create]
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.activity = @activity
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find(params[:activity_id])
+  end
+
+  def chatroom_params
+    params.require(:chatroom).permit(:title, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :activities, only: %i[index show]
+  resources :activities, only: %i[index show] do
+    resources :chatrooms, only: %i[new create]
+  end
+  resources :chatrooms, only: :show
 end

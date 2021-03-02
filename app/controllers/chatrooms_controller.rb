5db1288dd28eadad74efb00a6b654e3d40a2cbe1
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-02 16:28:48 -0300

    creates chatroom with participants

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -9,6 +9,10 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.new(chatroom_params)
     @chatroom.activity = @activity
     if @chatroom.save
+      params[:chatroom]["user_ids"].reject(&:blank?).map { |a| a.to_i }.each do |user_id|
+        @participant = Participant.create(user_id: user_id, chatroom_id: @chatroom.id, status: "false")
+      end
+      @participant = Participant.create(user_id: current_user.id, chatroom_id: @chatroom.id, status: "true")
       redirect_to chatroom_path(@chatroom)
     else
       render :new
@@ -27,6 +31,6 @@ class ChatroomsController < ApplicationController
   end
 
   def chatroom_params
-    params.require(:chatroom).permit(:title, :description)
+    params.require(:chatroom).permit(:title, :description, :participants)
   end
 end

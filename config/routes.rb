8193b9e1509f400e39bf6b523201a45e00a31b4a
Eric Hoare <hoareinc@gmail.com>
2021-03-02 19:21:09 -0300

    event controller, show created

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :activities, only: %i[index show]
+  resources :activities, only: %i[index show] do
+    resources :events, only: %i[show]
+  end
 end

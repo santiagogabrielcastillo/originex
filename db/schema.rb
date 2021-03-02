039f176fb143e43d09801612b88361c8df2fad28
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-02 15:26:00 -0300

    adds participant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_172043) do
+ActiveRecord::Schema.define(version: 2021_03_02_182534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 2021_03_02_172043) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "participants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "chatroom_id", null: false
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_participants_on_chatroom_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
   create_table "user_interests", force: :cascade do |t|
     t.bigint "activity_id", null: false
     t.bigint "user_id", null: false
@@ -86,6 +96,8 @@ ActiveRecord::Schema.define(version: 2021_03_02_172043) do
   add_foreign_key "events", "chatrooms"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "participants", "chatrooms"
+  add_foreign_key "participants", "users"
   add_foreign_key "user_interests", "activities"
   add_foreign_key "user_interests", "users"
 end

f195142cb340c7dd6c62d88aeb0d6cd91238db65
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-02 14:21:47 -0300

    adds user_interest model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_151441) do
+ActiveRecord::Schema.define(version: 2021_03_02_172043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2021_03_02_151441) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "user_interests", force: :cascade do |t|
+    t.bigint "activity_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["activity_id"], name: "index_user_interests_on_activity_id"
+    t.index ["user_id"], name: "index_user_interests_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -77,4 +86,6 @@ ActiveRecord::Schema.define(version: 2021_03_02_151441) do
   add_foreign_key "events", "chatrooms"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "user_interests", "activities"
+  add_foreign_key "user_interests", "users"
 end

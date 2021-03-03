95bb581af84055f60dc466b0dda98df7a985555f
Eric Hoare <hoareinc@gmail.com>
2021-03-03 11:15:23 -0300

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :activities, only: %i[index show] do
-<<<<<<< HEAD
     resources :events, only: %i[show]
-=======
     resources :chatrooms, only: %i[new create]
   end
   resources :chatrooms, only: :show do
     resources :messages, only: :create
->>>>>>> master
   end
 end

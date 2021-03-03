4eca4f311320755cba8a84b06d279d4764c4ff36
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-03 16:21:22 -0300

    adds scroll feature

--- app/javascript/components/init_chatroom.js
+++ app/javascript/components/init_chatroom.js
@@ -1,15 +1,23 @@
 const cleanInput = () => {
   const userInput = document.getElementById("user-input");
+  const messagesDiv = document.getElementById("messages");
 
   if (userInput) {
+    window.onload = () => {
+      messagesDiv.scrollTo(0, document.getElementById("messages").scrollHeight + 5);
+    };
     userInput.addEventListener('keyup', (event) => {
       if (event.key === "Enter") {
         event.preventDefault();
-        console.log(userInput)
         userInput.value = "";
+        messagesDiv.scrollTo(0, document.getElementById("messages").scrollHeight + 5);
       };
     });
   };
 };
 
-export { cleanInput };
+const cleanInputForExport = () => {
+  setTimeout(cleanInput(), 500);
+};
+
+export { cleanInputForExport };

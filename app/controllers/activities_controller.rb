be4d31a73243680b770594ff21a79b5ca21089f0
ariel-waisburg <arielwaisburg@gmail.com>
2021-03-05 12:14:16 -0300

    pg_search at index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,14 +1,10 @@
 class ActivitiesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-    if current_user.user_interests.empty?
-      redirect_to user_interests_path
+    if params[:query].present?
+      @activities = Activity.search_over_activities(params[:query])
     else
-      if params[:query].present?
-        @activities = Activity.search_over_activities(params[:query])
-      else
-        @activities = current_user.activities
-      end
+      @activities = current_user.activities
     end
   end
 

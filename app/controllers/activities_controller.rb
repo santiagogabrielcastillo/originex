a29d2cfa6454a6ff4572bc0e3af3e64414ee8d9b
Eric Hoare <hoareinc@gmail.com>
2021-03-05 12:08:05 -0300

    added join chatroom to activity show

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -14,6 +14,7 @@ class ActivitiesController < ApplicationController
 
   def show
     @activity = Activity.find(params[:id])
+    @participant = Participant.new
     @events = Event.where(activity_id: @activity.id)
   end
 end

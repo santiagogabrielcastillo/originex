4b4c21986f82c86bccf20c84a8f6959bce26a2cb
ariel-waisburg <arielwaisburg@gmail.com>
2021-03-09 12:46:18 -0300

    fixing wrong changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    if params[:query].present?
-      @activities = Activity.search_over_title_and_cateogry(params[:query])
+    if params[:zone].present? || params[:query].present? 
+      @activities = Activity.search({
+        title_category: params[:query],
+        zone: params[:zone]
+      })
     else
       @activities = Activity.all
     end

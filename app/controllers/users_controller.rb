38341896a036524aa3020e8de52c299274072d4c
Eric Hoare <hoareinc@gmail.com>
2021-03-09 00:16:01 -0300

    added age method to users controller, fixed activity cards pictures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @chats = @user.participants.where(status: true)
   end
+
+  def age
+    now = Time.current
+    dob = @user.birthday
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
+  helper_method :age
 end

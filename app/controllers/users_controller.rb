d60bd999887215b576a75dd89de90aec1303cff2
Lucia diez <lupe.diez@gmail.com>
2021-03-09 12:45:38 -0300

    new link to user own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @chats = @user.participants.where(status: true)
+    render 'devise/show'
   end
 end
